62dcf1c1e1268d9410a6e07932c53e98d017a8ba
Bsk <abiksb@gmail.com>
2023-01-22 20:34:54 +0100

    Change REST controller to stateless

--- src/main/java/com/example/calculator/CalculatorControllerREST.java
+++ src/main/java/com/example/calculator/CalculatorControllerREST.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Vector;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -16,20 +14,12 @@ public class CalculatorControllerREST {
     private CalculatorString calculatorString;
 
     private final AtomicLong counter = new AtomicLong();
-    //Czy Controller może być stanowy, czy nie powinien?
-    private List<Double> previousResultsList = new Vector<>();
 
     @GetMapping("/calculator/calculateREST")
     public CalculatorRestRepresentation calculate(@RequestParam(value = "expression", defaultValue = "0") String
-                                                    expression, @RequestParam(value = "id", defaultValue = "") Integer previousResultId){
-
-        double previousResult = 0;
-
-        if (previousResultId != null && previousResultId != 0) {previousResult = previousResultsList.get(previousResultId - 1);}
+                                                    expression, @RequestParam(value = "previousResult", defaultValue = "0") Double previousResult){
 
         double result = calculatorString.calculate(expression, previousResult);
-
-        previousResultsList.add(result);
 
         return new CalculatorRestRepresentation(counter.incrementAndGet(), expression, result);
 
